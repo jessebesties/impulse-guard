ee7087abcf5d768faba7d299caf70fe185397bc8
jessebesties <137125199+jessebesties@users.noreply.github.com>
2025-02-22 11:17:50 -0500

    some refactoring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import take_save_photo_webcam
+from webcam import take_save_photo_webcam
 from gcp_face_recognition import face_detection
 
-filename = take_save_photo_webcam.take_save_photo_webcam()
-face_detection.detect_and_box_faces(filename, 1, 1)
+webcam_output = take_save_photo_webcam.take_save_photo_webcam()
+face_detection.detect_and_box_faces(webcam_output, 1, 1)
