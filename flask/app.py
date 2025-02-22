63596f219f792bd88d6be7cb941aeab2f9458e34
jessebesties <137125199+jessebesties@users.noreply.github.com>
2025-02-22 11:57:30 -0500

    some refactoring

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response
+from webcam import live_video_feed
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def purchase():
 @app.route('/success')
 def success():
     return render_template('success.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(live_video_feed.generate_display_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from webcam import take_save_photo_webcam
 from gcp_face_recognition import face_detection
 
-webcam_output = take_save_photo_webcam.take_save_photo_webcam()
-face_detection.detect_and_box_faces(webcam_output, 1, 1)
+def take_picture_webcam():
+    webcam_output = take_save_photo_webcam.take_save_photo_webcam()
+    if webcam_output:
+        face_detection.detect_and_box_faces(webcam_output, 1, 1)
+
+face_detection.detect_and_box_faces("pictureofpeople.png", 999, 1)
